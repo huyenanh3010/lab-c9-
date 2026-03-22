df00ae841bb47100736eb0f743cab434b09a2c8f
huyenanh3010 <nguyenhuyenanhac2025@gmail.com>
2026-03-23 00:18:08 +0700

    upload lab c9

---
+++ Assets/Scripts/Lab 2/GameManager_Lab2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameManager_Lab2 : MonoBehaviour
+{
+    public static GameManager_Lab2 instance;
+
+    public int score = 100;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log("GameManager_Lab2 đang tồn tại, Score = " + score);
+    }
+}
